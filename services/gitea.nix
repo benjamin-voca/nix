f08fe3f75a3dd76fab6984fee520362cddf14211
Benjamin Voca <benjaminvocaa@gmail.com>
2026-01-22 20:58:02 +0100

    ig

--- services/gitea.nix
+++ services/gitea.nix
@@ -2,5 +2,19 @@
 
 {
   disabledModules = [ "services/misc/gitea.nix" ];
-  services.gitea.enable = true;
+  services.gitea = {
+    enable = true;
+    settings = {
+      server = {
+        DOMAIN = "git.quadtech.dev";
+        ROOT_URL = "https://git.quadtech.dev";
+        HTTP_PORT = 443;
+        SSH_PORT = 2222;
+        DISABLE_SSH = false;
+      };
+      service = {
+        DISABLE_REGISTRATION = false;
+      };
+    };
+  };
 }
