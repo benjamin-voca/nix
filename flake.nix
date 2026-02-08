bdba665af85f418ca713fb54133d80106bc7d2a6
Benjamin Voca <Benjamin-MacBook-Pro.local>
2026-02-08 16:53:08 +0100

    Define argocdChart in flake specialArgs

--- flake.nix
+++ flake.nix
@@ -24,11 +24,82 @@
   outputs = inputs:
     let
       lib = inputs.nixpkgs.lib;
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = lib.genAttrs systems;
+      helmLibFor = system:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+        in
+          import ./lib/helm {
+            inherit (inputs) nixhelm nix-kube-generators;
+            inherit pkgs system;
+          };
+      argocdChartFor = system:
+        let
+          helmLib = helmLibFor system;
+        in
+          helmLib.buildChart {
+            name = "argocd";
+            chart = helmLib.charts.argoproj.argo-cd;
+            namespace = "argocd";
+            values = {
+              global = {
+                domain = "argocd.quadtech.dev";
+              };
+
+              configs = {
+                cm = {
+                  "server.insecure" = true;
+                  url = "https://argocd.quadtech.dev";
+                };
+                params = {
+                  "server.insecure" = true;
+                };
+                secret = {
+                  argocdServerAdminPassword = "$2a$10$bX.6MmE5x1n.KlTA./3ax.xXzgP5CzLu1CyFyvMnEeh.vN9tDVVLC";
+                };
+              };
+
+              server = {
+                replicas = 1;
+                service = {
+                  type = "ClusterIP";
+                };
+              };
+
+              redis = {
+                enabled = true;
+              };
+
+              redis-ha = {
+                enabled = false;
+              };
+
+              controller = {
+                replicas = 1;
+              };
+
+              repoServer = {
+                replicas = 1;
+              };
+
+              applicationSet = {
+                enabled = true;
+              };
+
+              notifications = {
+                enabled = true;
+              };
+
+              global.image.tag = "v2.9.3";
+            };
+          };
       flakeOutputs = {
-        helmLib = import ./modules/outputs/helm.nix { inherit inputs lib; };
+        helmLib = forAllSystems (system: helmLibFor system);
+        argocdChart = forAllSystems (system: argocdChartFor system);
       };
       eval = lib.evalModules {
-        specialArgs = { inherit inputs; };
+        specialArgs = { inherit inputs; argocdChart = flakeOutputs.argocdChart; };
         modules = [
           ./modules/top.nix
         ];
--- modules/services/argocd-deploy.nix
+++ modules/services/argocd-deploy.nix
@@ -1,67 +1,11 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, argocdChart, ... }:
 
 let
   cfg = config.services.quadnix.argocd-deploy;
   kubectl = "${pkgs.kubectl}/bin/kubectl";
 
-  helmLib = config.flake.helmLib.${pkgs.stdenv.system};
-
-  argocdManifests = helmLib.buildChart {
-    name = "argocd";
-    chart = helmLib.charts.argoproj.argo-cd;
-    namespace = "argocd";
-    values = {
-      global = {
-        domain = "argocd.quadtech.dev";
-      };
-
-      configs = {
-        cm = {
-          "server.insecure" = true;
-          url = "https://argocd.quadtech.dev";
-        };
-        params = {
-          "server.insecure" = true;
-        };
-        secret = {
-          argocdServerAdminPassword = "$2a$10$bX.6MmE5x1n.KlTA./3ax.xXzgP5CzLu1CyFyvMnEeh.vN9tDVVLC";
-        };
-      };
-
-      server = {
-        replicas = 1;
-        service = {
-          type = "ClusterIP";
-        };
-      };
-
-      redis = {
-        enabled = true;
-      };
-
-      redis-ha = {
-        enabled = false;
-      };
-
-      controller = {
-        replicas = 1;
-      };
-
-      repoServer = {
-        replicas = 1;
-      };
-
-      applicationSet = {
-        enabled = true;
-      };
-
-      notifications = {
-        enabled = true;
-      };
-
-      global.image.tag = "v2.9.3";
-    };
-  };
+  system = pkgs.stdenv.system;
+  argocdManifests = argocdChart.${system};
 
   deploySh = pkgs.writeShellApplication {
     name = "argocd-deploy";
