056ab5020ab0b8c135b7f5c05cf58162c1edcf5e
Benjamin Voca <Benjamin-MacBook-Pro.local>
2026-02-08 16:49:17 +0100

    Pass flake to modules

--- modules/top.nix
+++ modules/top.nix
@@ -1,8 +1,9 @@
-{ inputs, lib, ... }:
+{ inputs, flake, lib, ... }:
 
 let
   imports = import ./imports.nix { inherit lib; };
 in
 {
   imports = imports;
+  _module.args = { inherit inputs flake; };
 }
