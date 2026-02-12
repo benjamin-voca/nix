58343665fbd63d1a617785bfe903074e1ee40488
Benjamin Voca <Benjamin-MacBook-Pro.local>
2026-02-12 23:04:10 +0100

    fix: correct chart paths (metallb.metallb, kubernetes-ingress-nginx.ingress-nginx)

--- modules/outputs/bootstrap.nix
+++ modules/outputs/bootstrap.nix
@@ -35,7 +35,7 @@ let
       metallbChart = pkgs.lib.pipe
         {
           name = "metallb";
-          chart = charts.metallb;
+          chart = charts.metallb.metallb;
           namespace = "metallb";
           values = {
             # IP address pool for LoadBalancer services
@@ -58,7 +58,7 @@ let
       ingressNginxChart = pkgs.lib.pipe
         {
           name = "ingress-nginx";
-          chart = charts.kubernetes-ingress-nginx;
+          chart = charts.kubernetes-ingress-nginx.ingress-nginx;
           namespace = "ingress-nginx";
           values = {
             controller = {
