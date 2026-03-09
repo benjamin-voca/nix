01bb0d9c42ac07d57fe8bddf116eb9fc96f1f917
Benjamin Voca <Benjamin-MacBook-Pro.local>
2026-03-09 21:14:00 +0100

    Downgrade infisical chart to 1.0.8 and configure external postgres

--- lib/helm/charts/infisical.nix
+++ lib/helm/charts/infisical.nix
@@ -4,8 +4,8 @@ let
   chart = helmLib.kubelib.downloadHelmChart {
     repo = "https://dl.cloudsmith.io/public/infisical/helm-charts/helm/charts/";
     chart = "infisical-standalone";
-    version = "1.7.2";
-    chartHash = "sha256-Rl9S/7vV4igSXzc4V/OeKTjEO2QjRyUkET9GDawM9Pk=";
+    version = "1.0.8";
+    chartHash = "sha256-gyDsPRxGY3U/7Qv9Z3Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9=";
   };
 in
 {
@@ -30,6 +30,13 @@ in
 
       postgresql = {
         enabled = false;
+        useExistingPostgresSecret = {
+          enabled = true;
+          existingConnectionStringSecret = {
+            name = "infisical-secrets";
+            key = "DB_CONNECTION_URI";
+          };
+        };
       };
 
       redis = {
