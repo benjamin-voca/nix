516a64ad070b72d96ac1066be31e4378921b81b8
Benjamin <benjamin.voca@quadtech.dev>
2026-03-28 12:15:02 +0100

    Restore OpenClaw token auth and fix JSON syntax

--- modules/outputs/bootstrap.nix
+++ modules/outputs/bootstrap.nix
@@ -1181,8 +1181,8 @@ data:
         "bind": "lan",
         "port": 18789,
         "auth": {
-          "mode": "none"
-        }
+          "mode": "token"
+        },
         "trustedProxies": [
           "10.0.0.0/8",
           "192.168.0.0/16",
